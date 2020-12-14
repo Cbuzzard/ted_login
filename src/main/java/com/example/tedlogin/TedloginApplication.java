5c74a11ec09813ef02bf5653a6a118a853af7727
cbuzzard <corwin1998@gmail.com>
2020-12-13 23:38:12 -0500

    init commit

---
+++ src/main/java/com/example/tedlogin/TedloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.tedlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tedlogin/TedloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tedlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TedloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
