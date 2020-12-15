7f8841614588e4a14882d12b3c53aec74a0226e9
cbuzzard <corwin1998@gmail.com>
2020-12-14 23:29:59 -0500

    init models

---
+++ src/main/java/com/example/tedlogin/model/User.java
@@ -0,0 +1,88 @@
+package com.example.tedlogin.model;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @ElementCollection
+    private List<Integer> games = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(@NotBlank @Size(max = 20) String username, @NotBlank @Size(max = 50) @Email String email, @NotBlank @Size(max = 120) String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Integer> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Integer> games) {
+        this.games = games;
+    }
+
+    public void addGame(int id) {
+        if(!this.games.contains(id)) {
+            this.games.add(id);
+        }
+    }
+
+}
